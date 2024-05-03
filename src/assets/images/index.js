f72211f9b3474e0d13cd06c82ab71f0e96ccbee4
Shashwat Saran <shashwatsaran@Shashwats-MacBook-Pro.local>
2024-05-03 14:44:56 +0530

    added

---
+++ src/assets/images/index.js
@@ -0,0 +1,15 @@
+import hero from './hero.jpg'
+import meta from './meta.png'
+import shopify from './shopify.png'
+import Ai4bharat from './Ai4bharat.png'
+import tesla from './tesla.png'
+import logo from './logo.svg'
+
+export {
+    hero,
+    meta,
+    shopify,
+    Ai4bharat,
+    tesla,
+    logo
+}
